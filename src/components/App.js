c93dee9110d30f0bed8dfb59da611d326a4786eb
BURCHILL <jarodburchill@gmail.com>
2019-07-03 09:47:28 -0400

    Layout change

--- src/components/App.js
+++ src/components/App.js
@@ -14,24 +14,24 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 import Carousel from 'react-bootstrap/Carousel';
-import lakers1 from './img/lakers1.png';
-import lakers2 from './img/lakers2.png';
-import lakers3 from './img/lakers3.png';
-import plumcreek1 from './img/plumcreek1.png'
-import budget1 from './img/budget1.png';
-import budget2 from './img/budget2.png';
-import budget3 from './img/budget3.png';
-import car1 from './img/car1.png';
-import car2 from './img/car2.png';
-import car3 from './img/car3.png';
-import review1 from './img/review1.png';
-import review2 from './img/review2.png';
-import review3 from './img/review3.png';
-import qpuzzle1 from './img/qpuzzle1.png';
-import qpuzzle2 from './img/qpuzzle2.png';
-import pong1 from './img/pong1.png';
-import pong2 from './img/pong2.png';
-import pong3 from './img/pong3.png';
+import lakers1 from '../img/lakers1.png';
+import lakers2 from '../img/lakers2.png';
+import lakers3 from '../img/lakers3.png';
+import plumcreek1 from '../img/plumcreek1.png'
+import budget1 from '../img/budget1.png';
+import budget2 from '../img/budget2.png';
+import budget3 from '../img/budget3.png';
+import car1 from '../img/car1.png';
+import car2 from '../img/car2.png';
+import car3 from '../img/car3.png';
+import review1 from '../img/review1.png';
+import review2 from '../img/review2.png';
+import review3 from '../img/review3.png';
+import qpuzzle1 from '../img/qpuzzle1.png';
+import qpuzzle2 from '../img/qpuzzle2.png';
+import pong1 from '../img/pong1.png';
+import pong2 from '../img/pong2.png';
+import pong3 from '../img/pong3.png';
 
 function App() {
   const [activePage, setActivePage] = useState("home");
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
 
