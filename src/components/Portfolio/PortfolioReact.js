e6d3b9c8a450dfef38ed16b4540feb26e96c0639
BURCHILL <jarodburchill@gmail.com>
2019-07-05 13:49:03 -0400

    Added calculator

--- src/components/Portfolio/PortfolioReact.js
+++ src/components/Portfolio/PortfolioReact.js
@@ -4,14 +4,18 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 import PortfolioBurchillDevelopment from './PortfolioBurchillDevelopment';
+import Calculator from './Calculator/Calculator';
 
 const PortfolioReact = () => {
   return (
     <>
-      <Tab.Container defaultActiveKey="burchilldev">
+      <Tab.Container defaultActiveKey="calculator">
         <Row>
           <Col sm={3} md={3}>
             <Nav variant="pills" className="flex-column">
+              <Nav.Item>
+                <Nav.Link eventKey="calculator">React Calculator</Nav.Link>
+              </Nav.Item>
               <Nav.Item>
                 <Nav.Link eventKey="burchilldev">Burchill Development</Nav.Link>
               </Nav.Item>
@@ -19,6 +23,9 @@ const PortfolioReact = () => {
           </Col>
           <Col sm={9} md={7}>
             <Tab.Content>
+              <Tab.Pane eventKey="calculator">
+                <Calculator />
+              </Tab.Pane>
               <Tab.Pane eventKey="burchilldev">
                 <PortfolioBurchillDevelopment />
               </Tab.Pane>
